979775637eaaeac7dc80fc91ce83744dbe6dc4bc
lucahuerse <luca.huerse@googlemail.com>
2021-11-25 23:39:45 +0100

    test change

--- heatmap.py
+++ heatmap.py
@@ -3,6 +3,7 @@ from mplsoccer import Pitch, VerticalPitch
 
 pitch = Pitch(pitch_color='grass', line_color='white', stripe=False)
 fig, ax = pitch.draw(figsize=(8, 4))
+# test test test
 
 plt.show()
 
