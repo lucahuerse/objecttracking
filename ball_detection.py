2ebba8fd86066d27ece68e00b160e5d891d728eb
lucahuerse <luca.huerse@googlemail.com>
2021-11-26 20:28:28 +0100

    third commit

--- ball_detection.py
+++ ball_detection.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
+import csv
 
-cap = cv2.VideoCapture("table_soccer_yellow.mp4")
+cap = cv2.VideoCapture("table_soccer_yellow_full_length.mp4")
 # cap = cv2.VideoCapture(0)
 
 
 def detect_ball():
+    coords = []
     while True:
         ret, frame = cap.read()
         width = int(cap.get(3))
@@ -22,26 +24,42 @@ def detect_ball():
         result = cv2.bitwise_and(roi, roi, mask=mask)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        coords = []
+        
+        cv2.circle(result, (0, 0), 10, (0,0,255), cv2.FILLED)
+        cv2.circle(result, (476, 720), 10, (0,0,255), cv2.FILLED)
+        cv2.circle(result, (0, 720), 10, (0,0,255), cv2.FILLED)
+        cv2.circle(result, (476, 0), 10, (0,0,255), cv2.FILLED)
+
 
         for cnt in contours:
             area = cv2.contourArea(cnt)
-            if area > 30:
+            if area > 70:
                 c, r = cv2.minEnclosingCircle(cnt)
                 cx, cy = int(c[0]), int(c[1])
                 cv2.circle(result, (cx, cy), int(r), (0, 255, 0), 2)
                 cv2.circle(result, (cx, cy), 3, (0, 0, 255), cv2.FILLED)
-                coords = coords + [cx, cy]
-                print(coords)
+                coords = coords + [(cx, cy)]
 
         cv2.imshow("Region of Interest", roi)
         cv2.imshow("Result", result)
 
-        key = cv2.waitKey(30)
+        key = cv2.waitKey(1)
         if key == ord('q'):
             break
     return coords
 
-detect_ball()
+
+data = detect_ball()
 cap.release()
 cv2.destroyAllWindows()
+
+header = ['x', 'y']
+
+with open('data.csv', 'w', encoding='UTF8', newline='\n') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write multiple rows
+    writer.writerows(data)
--- heatmap.py
+++ heatmap.py
@@ -1,9 +1,40 @@
 import matplotlib.pyplot as plt
 from mplsoccer import Pitch, VerticalPitch
+import seaborn as sns
+import pandas as pd
 
-pitch = Pitch(pitch_color='grass', line_color='white', stripe=False)
-fig, ax = pitch.draw(figsize=(8, 4))
-# test test test
+df = pd.read_csv('data.csv')
 
+df['x'] = (df['x']/476)*80
+df['y'] = (df['y']/720)*120
+
+fig, ax = plt.subplots(figsize=(4, 6))
+fig.set_facecolor('#22312b')
+ax.patch.set_facecolor('#22312b')
+
+pitch = VerticalPitch(pitch_type='statsbomb', pitch_color='#22312b', line_color='white', stripe=False)
+pitch.draw(ax=ax)
+plt.gca().invert_yaxis()
+
+kde = sns.kdeplot(
+        df['x'],
+        df['y'],
+        shade = True,
+        shade_lowest=False,
+        alpha=.7,
+        n_levels=20,
+        cmap = 'magma'
+)
+
+
+
+#use a for loop to plot each pass
+# for x in range(len(df['x'])):
+#     plt.scatter(df['x'][x],df['y'][x],color='green')
+        
+# plt.xlim(0,120)
+# plt.ylim(0,80)
+
+plt.title('Heatmap Table Soccer', color='white', size=20)
 plt.show()
 
