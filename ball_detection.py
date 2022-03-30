10b6ccd4a7d8a1849c02974886783b0e26ebfa8a
lucahuerse <luca.huerse@gmail.com>
2022-03-30 16:24:42 +0200

    beautified code

--- ball_detection.py
+++ ball_detection.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 import csv
 
-cap = cv2.VideoCapture("table_soccer_yellow_full_length.mp4")
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("table_soccer_yellow.mp4")
 
 
 def detect_ball():
@@ -15,37 +14,39 @@ def detect_ball():
 
         roi = frame[0:height, int(width/3)+30:int(2*width/3)+80]
 
-        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)  # hue, saturation, lightness
-        lower_yellow = np.array([14, 130, 20])
-        upper_yellow = np.array([24, 255, 255])
+        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)  # hue, saturation, value
+        yellow_min = np.array([14, 130, 20])
+        yellow_max = np.array([24, 255, 255])
 
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        mask = cv2.inRange(hsv, yellow_min, yellow_max)
 
         result = cv2.bitwise_and(roi, roi, mask=mask)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        
-        cv2.circle(result, (0, 0), 10, (0,0,255), cv2.FILLED)
-        cv2.circle(result, (476, 720), 10, (0,0,255), cv2.FILLED)
-        cv2.circle(result, (0, 720), 10, (0,0,255), cv2.FILLED)
-        cv2.circle(result, (476, 0), 10, (0,0,255), cv2.FILLED)
-
+        cv2.drawContours(result, contours, -1, (0,255,0), 2)
 
         for cnt in contours:
             area = cv2.contourArea(cnt)
-            if area > 70:
+            if area >= 30:
                 c, r = cv2.minEnclosingCircle(cnt)
-                cx, cy = int(c[0]), int(c[1])
+                cx, cy = int(c[0]), int(c[1])                                       
                 cv2.circle(result, (cx, cy), int(r), (0, 255, 0), 2)
                 cv2.circle(result, (cx, cy), 3, (0, 0, 255), cv2.FILLED)
-                coords = coords + [(cx, cy)]
+                coords = coords + [(cx, cy)] 
 
-        cv2.imshow("Region of Interest", roi)
-        cv2.imshow("Result", result)
+                
+        combo_image = cv2.addWeighted(roi, 0.8, result, 1, 1)
 
-        key = cv2.waitKey(1)
+
+        cv2.imshow("region of interest", combo_image)
+
+
+        key = cv2.waitKey(30)
         if key == ord('q'):
             break
+    print(np.shape(frame))
+    print(np.shape(frame[0]))
+    print(np.shape(frame[0][0]))
+
     return coords
 
 
@@ -55,11 +56,12 @@ cv2.destroyAllWindows()
 
 header = ['x', 'y']
 
-with open('data.csv', 'w', encoding='UTF8', newline='\n') as f:
-    writer = csv.writer(f)
+# with open('data.csv', 'w', encoding='UTF8', newline='\n') as f:
+#     writer = csv.writer(f)
 
-    # write the header
-    writer.writerow(header)
+#     # write the header
+#     writer.writerow(header)
 
-    # write multiple rows
-    writer.writerows(data)
+#     # write multiple rows
+#     writer.writerows(data)
+
